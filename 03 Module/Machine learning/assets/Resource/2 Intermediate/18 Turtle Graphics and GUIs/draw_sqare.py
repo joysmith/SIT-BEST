ebc6368e977f403da0c89be19617e26a1bd965b7
joy smith peter <joysmith271@gmail.com>
2025-05-19 04:57:12 +0530

    r

--- 03 Module/Machine learning/assets/Resource/2 Intermediate/18 Turtle Graphics and GUIs/draw_sqare.py
+++ 03 Module/Machine learning/assets/Resource/2 Intermediate/18 Turtle Graphics and GUIs/draw_sqare.py
@@ -1,7 +1,23 @@
 import turtle as t
 
-timmy_the_turtle = t.Turtle()
+object_1 = t.Turtle()
+
+object_2 = t.Turtle()
+
+object_3 = t.Turtle()
+
+object_4 = t.Turtle()
+
 
 for _ in range(10):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
+    object_1.backward(50)
+    object_1.right(90)
+
+    object_2.forward(40)
+    object_2.right(90)
+
+    object_3.backward(30)
+    object_3.left(90)
+
+    object_4.forward(20)
+    object_4.left(90)
